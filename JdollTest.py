7b0098d2a2ce628b16d99361dfc793fde58e45e9
jd174 <17jareddoll@gmail.com>
2022-01-09 00:13:30 -0500

    commit

--- JdollTest.py
+++ JdollTest.py
@@ -1,1 +1,21 @@
-print('Hello world')
+from ursina import *
+
+
+# update is run every frame
+def update():
+	#print('test')
+	if held_keys['a']:
+		cube.x -= 1 * time.dt
+
+
+app = Ursina()
+
+
+cube = Entity(model='quad', color=color.orange, scale = (2,5), position = (5,1))
+
+
+
+
+
+
+app.run()
